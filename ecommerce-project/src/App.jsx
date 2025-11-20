ce7a239cfea1edd4a9de5e6ef0399975fcfcb675
Uthila Wickramage <uhwickramage@gmail.com>
2025-11-20 21:42:16 +0530

    moved all pages to the react project

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,19 @@
 import HomePage from './pages/HomePage.jsx'
+import {Route, Routes} from 'react-router'
 import './App.css'
+import CheckoutPage from './pages/CheckoutPage.jsx'
+import TrackingPage from './pages/TrackingPage.jsx'
+import OrdersPage from './pages/OrdersPage.jsx'
 
 function App() {
 
   return (
-    
-      <HomePage/>
+    <Routes>
+      <Route index element={<HomePage/>}/>
+      <Route path='/checkout'  element={<CheckoutPage/>}/>
+            <Route path='/tracking'  element={<TrackingPage/>}/>
+<Route path='/orders' element={<OrdersPage/>}/>
+    </Routes>
     
   )
 }
